0bedc54d43ec4c906efcc6b59e74756c418d76b3
Lucien Ritorto <lucien.ritorto@gmail.com>
2014-12-30 01:12:10 -0500

    we are turning this into a maze solving proving program...we added two functions xplor and printgrid and deleted some old stuff.

--- test.c
+++ test.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
-sum(x,y) {
-  return(x+y);
+
+printgrid(grid)
+  char grid[15][25];
+{
+  int x,y;
+  for(y=1;y<=12;y++)
+    {
+    for(x=0;x<=19;x++)
+      printf("%c",grid[y][x]);
+    printf("\n");
+    }
+  return 0;
+}
+
+xplor(grid)
+   char grid[15][25];
+{
+  grid[1][2]="@";
+  return 0;
 }
 
 main() {
-  int x=4, y=7;
+  int x=1,y=1;
+  FILE * fp;
+  char grid[15][25];
 
-  printf("%d + %d = %d\n",x,y,sum(x,y));
+  if (!(fp=fopen("m","r")))
+    perror("Error opening m.");
+
+  while(fgets(grid[y++],80,fp))
+    ;
+  printgrid(grid);
+  xplor(grid);
+  printgrid(grid);
+
+  printf("Welp, see ya.\n");
+  fclose(fp);
   return 0;
 }
