a525bb43a2da5c087c43f7acebd38dfc83c5461b
Lucien Ritorto <lucien.ritorto@gmail.com>
2014-12-30 17:13:45 -0500

    made new mazes

--- test.c
+++ test.c
@@ -1,64 +1,78 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 
-printgrid(grid)
-  char grid[15][25];
+printgrid(grid,maxy,maxx)
+  char grid[69][135];
+  int maxy,maxx;
 {
   int x,y;
-  for(y=1;y<=12;y++)
+  for(y=0;y<=maxy;y++)
     {
-    for(x=0;x<=19;x++)
+    for(x=0;x<=maxx;x++)
       printf("%c",grid[y][x]);
     printf("\n");
     }
+sleep(1);
   return 0;
 }
 
-xplor(y,x,grid)
+xplor(y,x,grid,maxy,maxx)
    int x,y;
-   char grid[15][25];
+   char grid[69][135];
+   int maxy,maxx;
 {
-  sleep(1);
 /*mark our spot@*/
   grid[y][x]='@';
-  printgrid(grid);
+  printgrid(grid,maxy,maxx);
+  printf("We're at %d vertical and %d horizontal.\n",y,x);
 /*check if we are at the end of the maze*/
-  if(y==11 && x==19)
+  if(y==maxy && x==maxx)
+    {
+    printf("Yay, we made it!\n");
     exit(0);
+    }
 /*check if we can go south*/
   if(grid[y+1][x]==' ')
-    xplor(y+1,x,grid);
+    xplor(y+1,x,grid,maxy,maxx);
 
 /*check if we can go east*/
   if(grid[y][x+1]==' ')
-    xplor(y,x+1,grid);
+    xplor(y,x+1,grid,maxy,maxx);
 
 /*check if we can go north*/
   if(grid[y-1][x]==' ')
-    xplor(y-1,x,grid);
+    xplor(y-1,x,grid,maxy,maxx);
 
 /*check if we can go west*/
   if(grid[y][x-1]==' ')
-    xplor(y,x-1,grid);
+    xplor(y,x-1,grid,maxy,maxx);
   grid[y][x]='&';
   return 0;
 }
 
 main() {
-  int x=1,y=1;
+  int x=0,y=0,maxy=0,maxx=0;
   FILE * fp;
-  char grid[15][25];
+  char grid[69][135];
 
-  if (!(fp=fopen("m","r")))
-    perror("Error opening m.");
+  if (!(fp=fopen("maze","r")))
+    perror("Error opening maze.");
 
-  while(fgets(grid[y++],80,fp))
-    ;
-  printf("I've just read in this maze:\n");
-  printgrid(grid);
+  while(fgets(grid[y],134,fp))
+    {
+    printf("%lu\n",strlen(grid[y]));
+    if(strlen(grid[y])-2 > maxx)
+      maxx = strlen(grid[y])-2;
+      y++;
+    }
+  maxy=--y;
+  printf("I've just read in a %d tall by %d wide maze:\n",maxy,maxx);
+  printgrid(grid,maxy,maxx);
   printf("So let's start!\n");
-  xplor(1,1,grid);
-  printgrid(grid);
+  xplor(1,1,grid,maxy,maxx);
+  printgrid(grid,maxy,maxx);
 
   printf("Welp, see ya.\n");
   fclose(fp);
